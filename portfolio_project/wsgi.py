c00d567731c2103a27d31e92ff5df2e0f8482959
dua-saif <duasaif.atd@gmail.com>
2025-08-03 03:14:46 +0400

    Second commit

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+
+User = get_user_model()
+
+try:
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("Duasaif", "duasaif.atd@gmail.com", "1234sameed@")
+        print("✅ Superuser created!")
+except OperationalError:
+    print("⚠ Database not ready yet — skipping superuser creation.")
